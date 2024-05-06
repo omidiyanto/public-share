99036ed002c4f2cae30b9edff8ef577617c5a074
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-07 01:55:11 +0700

    Create install-ngrok-linux.sh

---
+++ install-ngrok-linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -O https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+sudo tar -xvzf ~ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+
+echo "NEXT! ADD YOUR OWN AUTH_TOKEN" 
